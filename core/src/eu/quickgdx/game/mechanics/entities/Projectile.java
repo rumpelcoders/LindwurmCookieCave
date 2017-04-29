01a8efe5afc8e929ed40c96ab95e5dff3295f24e
Xifiggam <xifiggam@gmx.at>
2017-04-29 23:25:40 +0200

    Songs/BGmusic added

---
+++ core/src/eu/quickgdx/game/mechanics/entities/Projectile.java
@@ -0,0 +1,22 @@
+package eu.quickgdx.game.mechanics.entities;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+import eu.quickgdx.game.mechanics.World;
+
+/**
+ * Created by Veit on 29.04.2017.
+ */
+public abstract class Projectile extends MoveableObject {
+
+    public int direction;
+
+    public Projectile(Vector2 position, World world, int direction) {
+        super(position, world);
+    }
+
+    public void hit(GameObject object){
+        if(object)
+    }
+}

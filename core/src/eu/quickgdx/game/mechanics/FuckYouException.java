a6dff9f99e2a1a21579373e6400a75a51fe1bda9
Christian Zellot <christian@zellot.at>
2017-04-29 16:48:22 +0200

    added some constants

---
+++ core/src/eu/quickgdx/game/mechanics/FuckYouException.java
@@ -0,0 +1,9 @@
+package eu.quickgdx.game.mechanics;
+
+/**
+ * Created by Zelle on 29.04.2017.
+ */
+public class FuckYouException extends Throwable {
+    public FuckYouException(String s) {
+    }
+}

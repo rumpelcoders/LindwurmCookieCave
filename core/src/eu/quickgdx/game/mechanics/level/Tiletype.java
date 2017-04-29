cb9cc6e1e5252d314b055740c464e0d9a630cb18
Christian Zellot <christian.zellot@aau.at>
2017-04-29 17:59:12 +0200

    tiletyp enumextended

--- core/src/eu/quickgdx/game/mechanics/level/Tiletype.java
+++ core/src/eu/quickgdx/game/mechanics/level/Tiletype.java
@@ -1,8 +1,50 @@
 package eu.quickgdx.game.mechanics.level;
 
+import eu.quickgdx.game.Constants;
+
 /**
  * Created by Veit on 28.04.2017.
  */
 public enum Tiletype {
-    FLOOR, NONWALKABLE, WALL, WALL_END_LEFT, WALL_END_RIGHT, WALL_END_BOTH,CEILING_NSEW, CEILING_N, CEILING_E, CEILING_W, CEILING_S, CEILING_NE, CEILING_NS, CEILING_NSE, CEILING_NSW, CEILING_NW, CEILING_NWE, CEILING_SE, CEILING_SW, CEILING_SWE, CEILING_WE
+    FLOOR(Constants.ASSET_MAP_FLOOR, false),
+    NONWALKABLE(Constants.ASSET_MAP_CEILING_BORDERLESS_EMPTY),
+    WALL(Constants.ASSET_MAP_FLOOR_WALL),
+    WALL_END_LEFT(Constants.ASSET_MAP_FLOOR_WALL_ENDING_LEFT),
+    WALL_END_RIGHT(Constants.ASSET_MAP_FLOOR_WALL_ENDING_RIGHT),
+    WALL_END_BOTH(Constants.ASSET_MAP_FLOOR_WALL_ENDING_RIGHT_LEFT),
+    CEILING_NSEW(Constants.ASSET_MAP_CEILING_NSWE),
+    CEILING_N(Constants.ASSET_MAP_CEILING_N),
+    CEILING_E(Constants.ASSET_MAP_CEILING_E),
+    CEILING_W(Constants.ASSET_MAP_CEILING_W),
+    CEILING_S(Constants.ASSET_MAP_CEILING_S),
+    CEILING_NE(Constants.ASSET_MAP_CEILING_NE_EMPTY),
+    CEILING_NS(Constants.ASSET_MAP_CEILING_NS_EMPTY),
+    CEILING_NSE(Constants.ASSET_MAP_CEILING_NSE_EMPTY),
+    CEILING_NSW(Constants.ASSET_MAP_CEILING_NSW_EMPTY),
+    CEILING_NW(Constants.ASSET_MAP_CEILING_NW_EMPTY),
+    CEILING_NWE(Constants.ASSET_MAP_CEILING_NWE_EMPTY),
+    CEILING_SE(Constants.ASSET_MAP_CEILING_SE_EMPTY),
+    CEILING_SW(Constants.ASSET_MAP_CEILING_SW_EMPTY),
+    CEILING_SWE(Constants.ASSET_MAP_CEILING_SWE_EMPTY),
+    CEILING_WE(Constants.ASSET_MAP_CEILING_WE_EMPTY);
+
+    private final boolean collision;
+    private final String assetPath;
+
+    Tiletype(String assetPath) {
+        this(assetPath, true);
+    }
+
+    Tiletype(String assetPath, boolean collision) {
+        this.assetPath = assetPath;
+        this.collision = collision;
+    }
+
+    public boolean isCollision() {
+        return collision;
+    }
+
+    public String getAssetPath() {
+        return assetPath;
+    }
 }

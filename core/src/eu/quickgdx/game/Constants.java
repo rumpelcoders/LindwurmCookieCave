20278e6acc6d7f6b81404c67dbb7b59a2101b7c1
Christian Zellot <christian.zellot@aau.at>
2017-04-29 09:43:43 +0200

    delete working files

--- core/src/eu/quickgdx/game/Constants.java
+++ core/src/eu/quickgdx/game/Constants.java
@@ -9,6 +9,9 @@ public class Constants {
     public final static String ASSET_PLAYER = "gameplay/test_char_64.png";
     public static final String ASSET_MAP_GROUND = "level/floor.png";
     public static final String ASSET_MAP_CEILING_W = "level/ceiling_w.png";
+    public static final String ASSET_MAP_CEILING_E = "level/ceiling_e.png";
+    public static final String ASSET_MAP_CEILING_S = "level/ceiling_s.png";
+    public static final String ASSET_MAP_CEILING_N = "level/ceiling_n.png";
 
     public final static int TILESIZE = 64;
     public final static float SCALE = 1.0f;

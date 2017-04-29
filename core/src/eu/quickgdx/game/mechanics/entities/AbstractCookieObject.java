2d3f78308a4703683985857ef1135510561df509
Christian Zellot <christian.zellot@aau.at>
2017-04-29 13:57:56 +0200

    cookie asset

--- core/src/eu/quickgdx/game/mechanics/entities/AbstractCookieObject.java
+++ core/src/eu/quickgdx/game/mechanics/entities/AbstractCookieObject.java
@@ -1,5 +1,6 @@
 package eu.quickgdx.game.mechanics.entities;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
@@ -13,14 +14,17 @@ import eu.quickgdx.game.mechanics.World;
 
 public abstract class AbstractCookieObject extends MovableCollisionObject {
 
+    private final Texture cookieTexture;
+
     public AbstractCookieObject(Vector2 position, World world) {
         super(position, world);
-        int boundsSize = Constants.TILESIZE - Constants.TILESIZE / 3;
-        this.bounds = new Rectangle(position.x + Constants.TILESIZE, position.y, boundsSize, boundsSize);
+        int boundsSize = Constants.TILESIZE - Constants.TILESIZE / 4;
+        this.bounds = new Rectangle(position.x, position.y, boundsSize, boundsSize);
+        cookieTexture = world.gameplayScreen.parentGame.getAssetManager().get(Constants.ASSET_COOKIE_BAG, Texture.class);
     }
 
     @Override
     public void render(float delta, SpriteBatch spriteBatch) {
-
+        spriteBatch.draw(cookieTexture, position.x, position.y);
     }
 }
